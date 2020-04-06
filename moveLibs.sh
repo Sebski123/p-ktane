dee2f61f7c9c0fd6f7f418d956c875743e190c4c
Sebastian <sebthomle@gmail.com>
2020-04-06 19:22:29 +0200

    Added prebuild task

---
+++ moveLibs.sh
@@ -0,0 +1,3 @@
+#Moves Folders from Modules/Libraries to Arduino Library location
+cd Modules/Libraries
+cp -r DSerial/ KTANECommon/ LedControl/ NeoICSerial/ C:/Users/sebth/Documents/Arduino/libraries
