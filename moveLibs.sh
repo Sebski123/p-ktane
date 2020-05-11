c0bfbe10be9f35274231c3e3e6283a319fc17e5e
Sebastian <sebthomle@gmail.com>
2020-05-11 22:01:18 +0200

    prebuild script should now work on other pc's

--- moveLibs.sh
+++ moveLibs.sh
@@ -1,3 +1,3 @@
 #Moves Folders from Modules/Libraries to Arduino Library location
 cd Modules/Libraries
-cp -r DSerial/ KTANECommon/ LedControl/ NeoICSerial/ MAX6954/ C:/Users/sebth/Documents/Arduino/libraries
+cp -r DSerial/ KTANECommon/ LedControl/ NeoICSerial/ MAX6954/ $HOME/Documents/Arduino/libraries
