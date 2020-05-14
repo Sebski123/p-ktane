44afb2f7334577b0a995a8056835abd2fa327dc2
Sebastian <sebthomle@gmail.com>
2020-05-14 15:05:10 +0200

    fixed formatting

--- Modules/mazeModule/mazeModule.ino
+++ Modules/mazeModule/mazeModule.ino
@@ -62,7 +62,7 @@ unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc = LedControl(DATA_PIN,CLOCK_PIN,LOAD_PIN,1);
+LedControl lc = LedControl(DATA_PIN, CLOCK_PIN, LOAD_PIN, 1);
 NeoICSerial serial_port;
 DSerialClient client(serial_port, 0x02);
 KTANEModule module(client, 2, 3);
@@ -98,78 +98,80 @@ void move(int direction)
   switch (direction)
   {
 
-    case UP:
+  case UP:
     Serial.println("up");
     if (mazeHorizontalWalls[mazeNum][playerLocation[0]][playerLocation[1]])
     {
-        module.strike();
+      module.strike();
       Serial.println("Strike");
     }
     else
     {
       lc.setLed(0, playerLocation[0], playerLocation[1], false);
-        playerLocation[1] -= 1;
+      playerLocation[1] -= 1;
       lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-      }
-      break;
-    
-    case DOWN:
+    }
+    break;
+
+  case DOWN:
     Serial.println("down");
     if (mazeHorizontalWalls[mazeNum][playerLocation[0]][playerLocation[1] + 1])
     {
-        module.strike();
+      module.strike();
       Serial.println("Strike");
     }
     else
     {
       lc.setLed(0, playerLocation[0], playerLocation[1], false);
-        playerLocation[1] += 1;
+      playerLocation[1] += 1;
       lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-      }
-      break;
-    
-    case LEFT:
+    }
+    break;
+
+  case LEFT:
     Serial.println("left");
     if (mazeVerticalWalls[mazeNum][playerLocation[1]][playerLocation[0]])
     {
-        module.strike();
+      module.strike();
       Serial.println("Strike");
     }
     else
     {
       lc.setLed(0, playerLocation[0], playerLocation[1], false);
-        playerLocation[0] -= 1;
+      playerLocation[0] -= 1;
       lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-      }
-      break;
-    
-    case RIGHT:
+    }
+    break;
+
+  case RIGHT:
     Serial.println("right");
     if (mazeVerticalWalls[mazeNum][playerLocation[1]][playerLocation[0] + 1])
     {
-        module.strike();
+      module.strike();
       Serial.println("Strike");
     }
     else
     {
       lc.setLed(0, playerLocation[0], playerLocation[1], false);
-        playerLocation[0] += 1;
+      playerLocation[0] += 1;
       lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-      }
-      break;
-    
-    default:
-      break;
+    }
+    break;
+
+  default:
+    break;
   }
   return;
 }
 
-void setup() {
+void setup()
+{
   serial_port.begin(19200);
   Serial.begin(19200);
 
   Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 0));
   while (!module.getConfig())
+  {
     module.interpretData();
   }
   Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 1));
@@ -180,9 +182,9 @@ void setup() {
   Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 3));
   // 8x8 Led-matrix setup
   pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN,  OUTPUT);
-  pinMode(LOAD_PIN,   OUTPUT);
-  
+  pinMode(CLOCK_PIN, OUTPUT);
+  pinMode(LOAD_PIN, OUTPUT);
+
   //Directional-buttons setup
   pinMode(UP_BTN, INPUT_PULLUP);
   pinMode(DOWN_BTN, INPUT_PULLUP);
@@ -193,9 +195,9 @@ void setup() {
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
-  lc.shutdown(0,false);
+  lc.shutdown(0, false);
   /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
+  lc.setIntensity(0, 8);
   /* and clear the display */
   lc.clearDisplay(0);
 
@@ -227,8 +229,8 @@ void setup() {
   Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 9));
   while ((goalLocation[0] == playerLocation[0]) && (goalLocation[1] == playerLocation[1]))
   {
-    goalLocation[0] = random(0,6);
-    goalLocation[1] = random(0,6);
+    goalLocation[0] = random(0, 6);
+    goalLocation[1] = random(0, 6);
   }
 
   Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 10));
@@ -243,49 +245,50 @@ void setup() {
   module.sendReady();
 }
 
-void loop() {
+void loop()
+{
   module.interpretData();
 
   if (!module.is_solved)
   {
-  //Blink maze markers and player location with different intervals
-  currentMillis = millis();
-  
+    //Blink maze markers and player location with different intervals
+    currentMillis = millis();
+
     if (currentMillis - previousPlayerMillis >= 100)
     {
       // save the last time you blinked the player
       if (markerBlinkCount >= 5)
       {
-    markerBlinkState = !markerBlinkState;
+        markerBlinkState = !markerBlinkState;
         markerBlinkCount = 0;
       }
       else
       {
         markerBlinkCount++;
-  }
+      }
 
-    previousPlayerMillis = currentMillis;
+      previousPlayerMillis = currentMillis;
       playerBlinkState = !playerBlinkState;
 
-  lc.setLed(0, markerLocations[0][0], markerLocations[0][1], markerBlinkState);
-  lc.setLed(0, markerLocations[1][0], markerLocations[1][1], markerBlinkState);
-  lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
+      lc.setLed(0, markerLocations[0][0], markerLocations[0][1], markerBlinkState);
+      lc.setLed(0, markerLocations[1][0], markerLocations[1][1], markerBlinkState);
+      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
     }
 
 
     if (millis() - lastDebounceTime >= 200)
     {
-    
-    //Read button press
-    dir = getBtnDir();
+
+      //Read button press
+      dir = getBtnDir();
       if (dir >= 0)
       {
         lastDebounceTime = millis();
         Serial.print("Pressed: ");
         Serial.println(dir);
 
-    //Try to move the player, gave a strike if a wall is hit
-    move(dir);
+        //Try to move the player, gave a strike if a wall is hit
+        move(dir);
 
         Serial.println((__FlashStringHelper *)pgm_read_word(string_table + 8));
         Serial.print(playerLocation[0]);
@@ -302,6 +305,6 @@ void loop() {
       {
         delayWithUpdates(module, 10);
       }
-    } 
+    }
   }
 }
