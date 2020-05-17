e4522a5227d6bdd20965ab2235f121e8e87bc0ca
Sebastian <sebthomle@gmail.com>
2020-05-17 22:40:39 +0200

    Added code for rgb leds and finished module logic

--- Modules/buttonModule/button.ino
+++ Modules/buttonModule/button.ino
@@ -12,12 +12,12 @@
 #define LOAD_PIN 12
 #define DATA_IN_PIN 13
 
-#define BUTTON_LED_PIN_GREEN 14 //A0
-#define BUTTON_LED_PIN_BLUE 15  //A1
-#define BUTTON_LED_PIN_RED 16   //A2
-#define STRIP_LED_PIN_GREEN 17  //A3
-#define STRIP_LED_PIN_BLUE 18   //A4
-#define STRIP_LED_PIN_RED 19    //A5
+#define BUTTON_LED_PIN_GREEN 14
+#define BUTTON_LED_PIN_BLUE 15
+#define BUTTON_LED_PIN_RED 16
+#define STRIP_LED_PIN_GREEN 17
+#define STRIP_LED_PIN_BLUE 18
+#define STRIP_LED_PIN_RED 19
 
 #define DISP_SINGLE(x, y) maxSingle((x), (y), LOAD_PIN, CLOCK_PIN, DATA_IN_PIN)
 
@@ -28,7 +28,7 @@ KTANEModule module(client, 2, 3);
 int buttonColor;
 int stripColor;
 int text;
-bool stripOn = true;
+bool stripOn = false;
 bool wasButtonHeld = false;
 
 int buttonState = 0;     // current state of the button
@@ -49,11 +49,19 @@ int colors[5][2] = {
     {4, 0}  //Transparent (buttons only, not  coloured strips)
 };
 
+int rgbValues[5][3] = {
+    {0, 0, 255},        //Blue
+    {255, 0, 0},        //Red
+    {255, 255, 255},    //White
+    {255, 255, 0},      //Yellow
+    {0, 0, 0}           //Transparent (buttons only, not  coloured strips)
+};
+
 int constants[4] = {
-    0b10111110, // 0
-    0b00010010, // 1
-    0b11011100, // 2
-    0b11011010  // 3
+    0b10111110, // 0    Abort
+    0b00010010, // 1    Detonate
+    0b11011100, // 2    Hold
+    0b11011010  // 3    Press
 };
 
 char solutionCheckTime[5];
@@ -63,18 +71,32 @@ void generateButton()
     buttonColor = colors[random(5)][0];
     stripColor = colors[random(4)][0];
     text = random(4);
+    analogWrite(BUTTON_LED_PIN_RED, rgbValues[buttonColor][0]);
+    analogWrite(BUTTON_LED_PIN_GREEN, rgbValues[buttonColor][1]);
+    analogWrite(BUTTON_LED_PIN_BLUE, rgbValues[buttonColor][2]);
 }
 
 void enableStrip()
 {
+    analogWrite(STRIP_LED_PIN_RED, rgbValues[stripColor][0]);
+    analogWrite(STRIP_LED_PIN_GREEN, rgbValues[stripColor][1]);
+    analogWrite(STRIP_LED_PIN_BLUE, rgbValues[stripColor][2]);
     stripOn = true;
+}
+
+void disableStrip()
+{
+    digitalWrite(STRIP_LED_PIN_GREEN, LOW);
+    digitalWrite(STRIP_LED_PIN_BLUE, LOW);
+    digitalWrite(STRIP_LED_PIN_RED, LOW);
+    stripOn = false;
 }
 
 void checkSolution(bool held)
 {
-    if(!held)
+    if (!held)
     {
-        if(text == 1 || (text == 2 && buttonColor == 1))
+        if (text == 1 || (text == 2 && buttonColor == 1))
         {
             module.win();
             return;
@@ -82,33 +104,28 @@ void checkSolution(bool held)
         else
         {
             module.strike();
-            wasButtonHeld = false;
             return;
         }
-        
     }
     else
     {
-        if(!((text == 1 || (text == 2 && buttonColor == 1))))
-        module.sendTime();
-        delay(100);
-        memcpy(solutionCheckTime, module.getTime(), 5);
-        if(!!((int)strchr(solutionCheckTime, colors[stripColor][1] + '0')))
+        if (!(text == 1 || (text == 2 && buttonColor == 1)))
         {
-            module.win();
-            return;
+            module.sendTime();
+            delay(100);
+            memcpy(solutionCheckTime, module.getTime(), 5);
+            if (!!((int)strchr(solutionCheckTime, colors[stripColor][1] + '0')))
+            {
+                module.win();
+                return;
+            }
         }
-        else
-        {
-            module.strike();
-            wasButtonHeld = false;
-            return;
-        }
+        module.strike();
+        wasButtonHeld = false;
+        stripColor = colors[random(4)][0];
+        disableStrip();
+        return;
     }
-    
-    
-    //module.win();
-    //module.strike();
 }
 
 void setup()
@@ -120,8 +137,12 @@ void setup()
     pinMode(LOAD_PIN, OUTPUT);
     pinMode(CLOCK_PIN, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
-    pinMode(BUTTON_LED_PIN, INPUT);
-    pinMode(STRIP_LED_PIN, INPUT);
+    pinMode(BUTTON_LED_PIN_GREEN, OUTPUT);
+    pinMode(BUTTON_LED_PIN_BLUE, OUTPUT);
+    pinMode(BUTTON_LED_PIN_RED, OUTPUT);
+    pinMode(STRIP_LED_PIN_GREEN, OUTPUT);
+    pinMode(STRIP_LED_PIN_BLUE, OUTPUT);
+    pinMode(STRIP_LED_PIN_RED, OUTPUT);
 
     DISP_SINGLE(max7219_reg_scanLimit, 0x07);
     DISP_SINGLE(max7219_reg_decodeMode, 0x00);  // using an led matrix (not digits)
@@ -137,8 +158,9 @@ void setup()
 
     // Generate button
     generateButton();
+
+    // Show text
     DISP_SINGLE(0, constants[text]);
-    //TO-DO: add button color led
 
     timer = millis();
 
@@ -154,7 +176,7 @@ void loop()
         // read the pushbutton input pin:
         buttonState = digitalRead(BUTTON_PIN);
 
-        if (!stripOn && millis() - timer > 1000)
+        if (buttonState && !stripOn && millis() - timer > 1000)
         {
             enableStrip();
             wasButtonHeld = true;
@@ -163,14 +185,9 @@ void loop()
         // compare the buttonState to its previous state
         if (buttonState != lastButtonState)
         {
-            // if the state has changed, increment the counter
-            if (buttonState == HIGH)
-            {
-                // if the current state is HIGH then the button went from off to on:
-                timer = millis();
-                stripOn = false;
-            }
-            else
+            timer = millis();
+
+            if (buttonState == LOW)
             {
                 // if the current state is LOW then the button went from on to off:
                 checkSolution(wasButtonHeld);
