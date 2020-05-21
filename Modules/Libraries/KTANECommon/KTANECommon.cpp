379f430ff481eba039e5d81219d90950256ac997
Sebastian <sebthomle@gmail.com>
2020-05-21 15:56:39 +0200

    Started updating KTANECommon to use SWire

--- Modules/Libraries/KTANECommon/KTANECommon.cpp
+++ Modules/Libraries/KTANECommon/KTANECommon.cpp
@@ -10,7 +10,8 @@
 #include "KTANECommon.h"
 #include <string.h>
 
-void config_to_raw(config_t *config, raw_config_t *raw_config) {
+void config_to_raw(config_t *config, raw_config_t *raw_config)
+{
   raw_config->ports = config->ports;
   raw_config->batteries = config->batteries;
   raw_config->indicators = config->indicators;
@@ -20,7 +21,8 @@ void config_to_raw(config_t *config, raw_config_t *raw_config) {
   raw_config->spacer2 = 1;
 }
 
-void raw_to_config(raw_config_t *raw_config, config_t *config) {
+void raw_to_config(raw_config_t *raw_config, config_t *config)
+{
   config->ports = raw_config->ports;
   config->batteries = raw_config->batteries;
   config->indicators = raw_config->indicators;
@@ -29,11 +31,13 @@ void raw_to_config(raw_config_t *raw_config, config_t *config) {
   config->serial[6] = '\0';
 }
 
-unsigned long config_to_seed(config_t *config){
+unsigned long config_to_seed(config_t *config)
+{
   unsigned long retval = 0;
   int i;
 
-  for(i = 5; i >= 0; i--){
+  for (i = 5; i >= 0; i--)
+  {
     retval = (retval << 6) + (int)(config->serial[i] - ' ');
   }
   retval = retval * (3 + config->ports);
@@ -42,54 +46,65 @@ unsigned long config_to_seed(config_t *config){
   return retval;
 }
 
-void saveTimeLeft(char *raw_config, char *config) {
+void saveTimeLeft(char *raw_config, char *config)
+{
   memcpy(config, raw_config, 4);
   config[4] = '\0';
 }
 
-void delayWithUpdates(KTANEModule &module, unsigned int length) {
+void delayWithUpdates(KTANEModule &module, unsigned int length)
+{
   unsigned long start_millis = millis();
-  while(millis() - start_millis < length){
+  while (millis() - start_millis < length)
+  {
     module.interpretData();
   }
 }
 
-void delayWithUpdates(KTANEController &controller, unsigned int length) {
+void delayWithUpdates(KTANEController &controller, unsigned int length)
+{
   unsigned long start_millis = millis();
-  while(millis() - start_millis < length){
+  while (millis() - start_millis < length)
+  {
     controller.interpretData();
   }
 }
 
-void putByte(byte data, int clock_pin, int data_pin) {
+void putByte(byte data, int clock_pin, int data_pin)
+{
   byte i = 8;
   byte mask;
-  while(i > 0) {
-    mask = 0x01 << (i - 1);      // get bitmask
-    digitalWrite(clock_pin, LOW);   // tick
-    if (data & mask){            // choose bit
-      digitalWrite(data_pin, HIGH);// send 1
-    }else{
+  while (i > 0)
+  {
+    mask = 0x01 << (i - 1);       // get bitmask
+    digitalWrite(clock_pin, LOW); // tick
+    if (data & mask)
+    {                               // choose bit
+      digitalWrite(data_pin, HIGH); // send 1
+    }
+    else
+    {
       digitalWrite(data_pin, LOW); // send 0
     }
-    digitalWrite(clock_pin, HIGH);   // tock
-    --i;                         // move to lesser bit
+    digitalWrite(clock_pin, HIGH); // tock
+    --i;                           // move to lesser bit
   }
 }
 
-void maxSingle(byte reg, byte col, int load_pin, int clock_pin, int data_pin) {
+void maxSingle(byte reg, byte col, int load_pin, int clock_pin, int data_pin)
+{
   //maxSingle is the "easy"  function to use for a single max7219
-  digitalWrite(load_pin, LOW);        // begin
-  putByte(reg, clock_pin, data_pin);  // specify register
-  putByte(col, clock_pin, data_pin);  // put data
-  digitalWrite(load_pin, LOW);        // and load da stuff
-  digitalWrite(load_pin,HIGH);
+  digitalWrite(load_pin, LOW);       // begin
+  putByte(reg, clock_pin, data_pin); // specify register
+  putByte(col, clock_pin, data_pin); // put data
+  digitalWrite(load_pin, LOW);       // and load da stuff
+  digitalWrite(load_pin, HIGH);
 }
 
-void(* softwareReset) (void) = 0;
+void (*softwareReset)(void) = 0;
 
-KTANEModule::KTANEModule(DSerialClient &dserial, int green_led_pin, 
-                         int red_led_pin):_dserial(dserial) {
+KTANEModule::KTANEModule(SWireClient &swire, int green_led_pin, int red_led_pin) : _swire(swire)
+{
   memset(&_config, 0, sizeof(config_t));
   memset(&_timeLeft, 0, sizeof(_timeLeft));
   _red_led_pin = red_led_pin;
@@ -102,16 +117,20 @@ KTANEModule::KTANEModule(DSerialClient &dserial, int green_led_pin,
   is_solved = 0;
 }
 
-void KTANEModule::interpretData(){
+void KTANEModule::interpretData(Stream &s)
+{
   char out_message[MAX_MSG_LEN];
   unsigned long start_millis;
-  
-  _dserial.doSerial();
-  if(_dserial.getData(out_message)) {
-    if(out_message[0] == CONFIG && strlen(out_message) == 8) {
+
+  if (_swire.getData(out_message))
+  {
+    if (out_message[0] == CONFIG && strlen(out_message) == 8)
+    {
       _got_config = 1;
       raw_to_config((raw_config_t *)(out_message + 1), &_config);
-    } else if(out_message[0] == RESET) {
+    }
+    else if (out_message[0] == RESET)
+    {
       // All of the stuff before softwareReset() is currently useless
       //  but is kept in case the hard-reset call is removed.
       is_solved = 0;
@@ -123,13 +142,54 @@ void KTANEModule::interpretData(){
 
       // Delay for a small bit to allow client to ACK the reset.
       start_millis = millis();
-      while(millis() - start_millis < 300){
-        _dserial.doSerial();
-      }
+      while (millis() - start_millis < 300){}
       softwareReset();
-    } else if(out_message[0] == NUM_STRIKES) {
+    }
+    else if (out_message[0] == NUM_STRIKES)
+    {
       _num_strikes = out_message[1];
-    } else if(out_message[0] == TIME) {
+    }
+    else if (out_message[0] == TIME)
+    {
+      saveTimeLeft((out_message + 1), _timeLeft);
+    }
+  }
+}
+
+void KTANEModule::interpretData()
+{
+  char out_message[MAX_MSG_LEN];
+  unsigned long start_millis;
+
+  if (_swire.getData(out_message))
+  {
+    if (out_message[0] == CONFIG && strlen(out_message) == 8)
+    {
+      _got_config = 1;
+      raw_to_config((raw_config_t *)(out_message + 1), &_config);
+    }
+    else if (out_message[0] == RESET)
+    {
+      // All of the stuff before softwareReset() is currently useless
+      //  but is kept in case the hard-reset call is removed.
+      is_solved = 0;
+      _num_strikes = 0;
+      _got_config = 0;
+      memset(&_config, 0, sizeof(config_t));
+      _got_reset = 1;
+      digitalWrite(_green_led_pin, LOW);
+
+      // Delay for a small bit to allow client to ACK the reset.
+      start_millis = millis();
+      while (millis() - start_millis < 300){}
+      softwareReset();
+    }
+    else if (out_message[0] == NUM_STRIKES)
+    {
+      _num_strikes = out_message[1];
+    }
+    else if (out_message[0] == TIME)
+    {
       saveTimeLeft((out_message + 1), _timeLeft);
     }
   }
@@ -137,7 +197,8 @@ void KTANEModule::interpretData(){
 
 // TODO: make non-blocking
 // currently will block non-communication code for 500ms
-int KTANEModule::strike() {
+int KTANEModule::strike()
+{
   int result = sendStrike();
   digitalWrite(_red_led_pin, HIGH);
   delayWithUpdates(*this, 500);
@@ -145,27 +206,32 @@ int KTANEModule::strike() {
   return result;
 }
 
-int KTANEModule::sendStrike() {
+int KTANEModule::sendStrike()
+{
   char str[2] = {STRIKE, '\0'};
-  return _dserial.sendData(str);
+  return _swire.sendData(str);
 }
 
-int KTANEModule::win() {
+int KTANEModule::win()
+{
   digitalWrite(_green_led_pin, HIGH);
   return sendSolve();
 }
 
-int KTANEModule::sendSolve() {
+int KTANEModule::sendSolve()
+{
   char str[2] = {SOLVE, '\0'};
   is_solved = 1;
-  return _dserial.sendData(str);
+  return _swire.sendData(str);
 }
 
 // TODO: maybe make non-blocking
-int KTANEModule::sendReady() {
+int KTANEModule::sendReady()
+{
   char str[2] = {READY, '\0'};
-  int result = _dserial.sendData(str);
-  if(result){
+  int result = _swire.sendData(str);
+  if (result)
+  {
     digitalWrite(_red_led_pin, LOW);
     digitalWrite(_green_led_pin, HIGH);
     delayWithUpdates(*this, 300);
@@ -174,199 +240,245 @@ int KTANEModule::sendReady() {
   return result;
 }
 
-int KTANEModule::sendDebugMsg(char *msg) {
-  return _dserial.sendData(msg);
+int KTANEModule::sendDebugMsg(char *msg)
+{
+  return _swire.sendData(msg);
 }
 
-config_t *KTANEModule::getConfig() {
-  if(_got_config){
+config_t *KTANEModule::getConfig()
+{
+  if (_got_config)
+  {
     return &_config;
-  } else {
+  }
+  else
+  {
     return NULL;
   }
 }
 
-int KTANEModule::sendTime() {
+int KTANEModule::sendTime()
+{
   char str[2] = {TIME, '\0'};
-  return _dserial.sendData(str);
+  return _swire.sendData(str);
 }
 
-char* KTANEModule::getTime() {
+char *KTANEModule::getTime()
+{
   return _timeLeft;
 }
 
-int KTANEModule::getLitFRK() {
+int KTANEModule::getLitFRK()
+{
   return _config.indicators & 1;
 }
 
-int KTANEModule::getLitCAR() {
+int KTANEModule::getLitCAR()
+{
   return _config.indicators & 2;
 }
 
-int KTANEModule::getNumBatteries() {
+int KTANEModule::getNumBatteries()
+{
   return _config.batteries;
 }
 
-int KTANEModule::getParallelPort() {
+int KTANEModule::getParallelPort()
+{
   return _config.ports & 1;
 }
 
-int KTANEModule::getRCAPort() {
+int KTANEModule::getRCAPort()
+{
   return _config.ports & 2;
 }
 
-int KTANEModule::getRJ45Port() {
+int KTANEModule::getRJ45Port()
+{
   return _config.ports & 4;
 }
 
-char KTANEModule::getSerialDigit(int index) {
-  if(index < 0 or index > 5){
+char KTANEModule::getSerialDigit(int index)
+{
+  if (index < 0 or index > 5)
+  {
     return (char)0;
   }
   return _config.serial[index];
 }
 
-int KTANEModule::serialContains(char c) {
+int KTANEModule::serialContains(char c)
+{
   return !!((int)strchr(_config.serial, c));
 }
 
-int KTANEModule::serialContainsVowel() {
+int KTANEModule::serialContainsVowel()
+{
   int serial_length = strlen(_config.serial);
   char temp;
-  for(int i = 0; i < serial_length; i++) {
+  for (int i = 0; i < serial_length; i++)
+  {
     temp = _config.serial[i];
-    if(temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U') {
+    if (temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U')
+    {
       return 1;
     }
   }
   return 0;
 }
 
-int KTANEModule::getNumStrikes() {
+int KTANEModule::getNumStrikes()
+{
   return _num_strikes;
 }
 
-int KTANEModule::getReset() {
-  if(_got_reset){
+int KTANEModule::getReset()
+{
+  if (_got_reset)
+  {
     _got_reset = 0;
     return 1;
   }
   return 0;
 }
 
-KTANEController::KTANEController(DSerialMaster &dserial):_dserial(dserial) {
+KTANEController::KTANEController(SWireMaster &swire) : _swire(swire)
+{
   memset(_strikes, 0, MAX_CLIENTS);
   memset(_solves, 0, MAX_CLIENTS);
   memset(_readies, 0, MAX_CLIENTS);
 }
-void KTANEController::setTime(unsigned long timeLeft) {
+
+void KTANEController::setTime(unsigned long timeLeft)
+{
   timeLeftOnTimer = timeLeft;
 }
-void KTANEController::interpretData() {
+
+void KTANEController::interpretData()
+{
   char out_message[MAX_MSG_LEN];
-  _dserial.doSerial();
-  int client_id = _dserial.getData(out_message);
-  if(client_id) {
-    if(out_message[0] == STRIKE) {
+  int client_id = _swire.getData(out_message);
+  if (client_id)
+  {
+    if (out_message[0] == STRIKE)
+    {
       _strikes[client_id] = _strikes[client_id] + 1;
       sendStrikes();
-    } else if(out_message[0] == SOLVE) {
+    }
+    else if (out_message[0] == SOLVE)
+    {
       _solves[client_id] = 1;
-    } else if(out_message[0] == READY) {
+    }
+    else if (out_message[0] == READY)
+    {
       _readies[client_id] = 1;
-    } else if(out_message[0] == TIME) {
+    }
+    else if (out_message[0] == TIME)
+    {
       char msg[6];
       msg[0] = TIME;
-      int seconds = (timeLeftOnTimer / 1000)%60;
+      int seconds = (timeLeftOnTimer / 1000) % 60;
       int minutes = timeLeftOnTimer / 60000;
       msg[1] = (minutes / 10) + '0';
       msg[2] = (minutes % 10) + '0';
       msg[3] = (seconds / 10) + '0';
       msg[4] = (seconds % 10) + '0';
       msg[5] = '\0';
-      _dserial.sendData(client_id, msg);
+      _swire.sendData(client_id, msg);
     }
   }
 }
 
-int KTANEController::sendConfig(config_t *config) {
+int KTANEController::sendConfig(config_t *config)
+{
   char msg[9];
   int err = 0;
   uint8_t clients[MAX_CLIENTS];
   int num_clients = 0;
-  num_clients = _dserial.getClients(clients);
+  num_clients = _swire.getClients(clients);
 
   msg[0] = CONFIG;
-  config_to_raw(config, (raw_config_t *)(msg+1));
+  config_to_raw(config, (raw_config_t *)(msg + 1));
   msg[8] = '\0';
 
-  for(int i = 0; i < num_clients; i++) {
-    if(!_dserial.sendData(clients[i], msg)) {
+  for (int i = 0; i < num_clients; i++)
+  {
+    if (!_swire.sendData(clients[i], msg))
+    {
       err++;
     }
-    _dserial.doSerial();
   }
   return (err == 0);
 }
 
-int KTANEController::getStrikes() {
+int KTANEController::getStrikes()
+{
   int num_strikes = 0;
-  for(int i = 0; i < MAX_CLIENTS; i++) {
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
     num_strikes += _strikes[i];
   }
   return num_strikes;
 }
 
-int KTANEController::getSolves() {
+int KTANEController::getSolves()
+{
   int num_solves = 0;
-  for(int i = 0; i < MAX_CLIENTS; i++) {
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
     num_solves += _solves[i];
   }
   return num_solves;
 }
 
-int KTANEController::clientsAreReady() {
+int KTANEController::clientsAreReady()
+{
   int num_readies = 0;
-  for(int i = 0; i < MAX_CLIENTS; i++) {
+  for (int i = 0; i < MAX_CLIENTS; i++)
+  {
     num_readies += _readies[i];
   }
-  return num_readies >= _dserial.getClients(NULL);
+  return num_readies >= _swire.getClients(NULL);
 }
 
-int KTANEController::sendReset() {
+int KTANEController::sendReset()
+{
   char msg[2] = {RESET, '\0'};
   int err = 0;
   uint8_t clients[MAX_CLIENTS];
   int num_clients = 0;
-  num_clients = _dserial.getClients(clients);
+  num_clients = _swire.getClients(clients);
 
-  for(int i = 0; i < num_clients; i++) {
-    if(!_dserial.sendData(clients[i], msg)) {
+  for (int i = 0; i < num_clients; i++)
+  {
+    if (!_swire.sendData(clients[i], msg)) 
+    {
       err++;
     }
-    _dserial.doSerial();
   }
   return (err == 0);
 }
 
-int KTANEController::sendStrikes() {
+int KTANEController::sendStrikes()
+{
   int num_strikes = getStrikes();
   char msg[3] = {NUM_STRIKES, (char)num_strikes, '\0'};
   int err = 0;
   uint8_t clients[MAX_CLIENTS];
   int num_clients = 0;
 
-  if(num_strikes > 0){
-    num_clients = _dserial.getClients(clients);
+  if (num_strikes > 0)
+  {
+    num_clients = _swire.getClients(clients);
 
-    for(int i = 0; i < num_clients; i++) {
-      if(!_dserial.sendData(clients[i], msg)) {
+    for (int i = 0; i < num_clients; i++)
+    {
+      if (!_swire.sendData(clients[i], msg))
+      {
         err++;
       }
-      _dserial.doSerial();
     }
     return (err == 0);
   }
   return 0;
 }
-
