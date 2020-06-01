2025b9270d558ed1e0568f065cf7562748d183f7
Sebastian <sebthomle@gmail.com>
2020-06-01 22:18:57 +0200

    Goal can no longer appear on marker locations

--- Modules/mazeModule/mazeModule.ino
+++ Modules/mazeModule/mazeModule.ino
@@ -197,7 +197,7 @@ void setup()
   Serial.println(playerLocation[1]);
 
   Serial.println(F("Choosing goal location"));
-  while ((goalLocation[0] == playerLocation[0]) && (goalLocation[1] == playerLocation[1]))
+  while (((goalLocation[0] == playerLocation[0]) && (goalLocation[1] == playerLocation[1])) && ((goalLocation[0] == markerLocations[0][0]) && (goalLocation[1] == markerLocations[0][1])) && ((goalLocation[0] == markerLocations[1][0]) && (goalLocation[1] == markerLocations[1][1])) )
   {
     goalLocation[0] = random(0, 6);
     goalLocation[1] = random(0, 6);
