98ab66e7332c13436d2c5181059f4a8d6a207487
Sebastian <sebthomle@gmail.com>
2020-06-01 22:16:30 +0200

    Added template module

---
+++ Modules/templateModule/module.ino
@@ -0,0 +1,35 @@
+#include "KTANECommon.h"
+
+SWireClient client(MY_ADDRESS);
+KTANEModule module(client, 3, 4);
+
+void setup() {
+  Serial.begin(19200);
+  
+  while(!module.getConfig()){
+    module.interpretData();
+  }
+
+  /*
+    Do setup here
+  */
+
+  module.sendReady();
+}
+
+void loop() {
+  module.interpretData();
+
+  if(!module.is_solved){
+    /*
+    checkInputs();
+    if(they_solved_it) {
+      module.win();
+    }
+    if(they_messed_up) {
+      module.strike();
+    }
+    updateOutputs();
+    */
+  }
+}
