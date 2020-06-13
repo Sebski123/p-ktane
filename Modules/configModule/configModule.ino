9c8f0797a00497d9d346870fbbea5a230f8baebb
Sebastian <sebthomle@gmail.com>
2020-06-13 23:28:42 +0200

    Can now send scores to firebase

--- Modules/configModule/configModule.ino
+++ Modules/configModule/configModule.ino
@@ -1,3 +1,4 @@
+#include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -9,14 +10,18 @@
 extern const char INDEX_HTML[];
 
 // Fill in your WiFi router SSID and password
-const char *ssid = "Sebastian";
-const char *password = "12341234";
+#define FIREBASE_HOST "p-ktane.firebaseio.com"
+#define FIREBASE_AUTH "IOSc03Pc3bdDasJCHNqLeZueGVEVHrJgSFLerxui"
+#define WIFI_SSID "KatjaKaj"
+#define WIFI_PASSWORD "Sebastianqaz"
 MDNSResponder mdns;
 
 ESP8266WebServer server(80);
+FirebaseData firebaseData;
 
 raw_config_t stored_config;
 int num_minutes;
+char timeLeft[5] = {0, 0, 0, 0, '\0'};
 
 void returnFail(String msg)
 {
@@ -82,6 +87,27 @@ void handleNotFound()
   server.send(404, "text/plain", message);
 }
 
+void submitTime(char *time)
+{
+  char path[14];
+  strcpy(path, "/Score/");
+  memcpy(path + 7, stored_config.serial, 5);
+  path[12] = stored_config.serial6 + '0';
+  path[13] = '\0';
+
+  float fTimeLeft = 0;
+  fTimeLeft += (time[0] - '0') * 10;
+  fTimeLeft += time[1] - '0';
+  fTimeLeft += (time[2] - '0') / 10.0;
+  fTimeLeft += (time[3] - '0') / 100.0;
+
+  if (!Firebase.pushFloat(firebaseData, path, fTimeLeft))
+  {
+    Serial.print("Error in push, ");
+    Serial.println(firebaseData.errorReason());
+  }
+}
+
 void setup(void)
 {
   Serial.begin(19200);
@@ -96,7 +122,7 @@ void setup(void)
   // Read time
   num_minutes = EEPROM.read(addr++);
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   //Serial.println("");
 
   // Wait for connection
@@ -107,9 +133,12 @@ void setup(void)
   }
   Serial.println("");
   Serial.print("Connected to ");
-  Serial.println(ssid);
+  Serial.println(WIFI_SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
 
   if (mdns.begin("ktane-setup", WiFi.localIP()))
   {
@@ -136,6 +165,15 @@ void loop(void)
   {
     while (Serial.available() > 0)
     {
+      if (Serial.read() == 'W')
+      {
+        char c;
+        for(int i = 0; i < 4; i++){
+          while(!Serial.available() > 0){}
+          timeLeft[i] = Serial.read();;
+        }
+        submitTime(timeLeft);
+      }
       // Throw away data
       Serial.read();
     }
