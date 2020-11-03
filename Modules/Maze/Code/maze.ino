7db80c43613487bca30e9afd91025b8f4cc2daa3
Sebastian <sebthomle@gmail.com>
2020-11-03 22:28:25 +0100

    Updated maze code to use HD107S leds

--- Modules/Maze/Code/maze.ino
+++ Modules/Maze/Code/maze.ino
@@ -1,7 +1,9 @@
 #include "SWire.h"
 #include "KTANECommon.h"
 #include "LedControl.h"
-#include "mazeModule.h"
+#include "Bounce2.h"
+#include "FastLED.h"
+#include "maze.h"
 
 /*                                                                                                                            
                                                     dddddddd                                                                 
@@ -32,276 +34,285 @@ UU:::::U     U:::::UU                               d:::::d                     
 
 // Defines
 //  Pins
-#define GREEN_CLEAR_LED 2
-#define RED_STRIKE_LED 3
-#define RIGHT_BTN 4
-#define UP_BTN 5
-#define DOWN_BTN 6
-#define LEFT_BTN 7
-#define CLOCK_PIN 10
-#define LOAD_PIN 11
-#define DATA_PIN 12
+#define RIGHT_BTN 2
+#define UP_BTN 3
+#define DOWN_BTN 4
+#define LEFT_BTN 5
+#define GREEN_CLEAR_LED 6
+#define RED_STRIKE_LED 7
+#define CLOCK_PIN 8
+#define DATA_PIN 9
 //I2C SDA 18
 //I2C SCL 19
 
+#define NUM_GRID_LEDS 144
+#define debounceTime 20
+
 //Class inits
-LedControl lc = LedControl(DATA_PIN, CLOCK_PIN, LOAD_PIN, 1);
 SWireClient client(0x07);
 KTANEModule module(client, GREEN_CLEAR_LED, RED_STRIKE_LED);
-
+Bounce up_db = Bounce();
+Bounce left_db = Bounce();
+Bounce down_db = Bounce();
+Bounce right_db = Bounce();
+CRGB dark = CRGB(4, 4, 8);        //color of unoccupied spaces
+CRGB white = CRGB(255, 255, 255); //color of player position
+CRGB red = CRGB(255, 0, 0);       //color of walls
+CRGB darkred = CRGB(25, 0, 0);    //color of debug walls
+CRGB circle = CRGB(0, 255, 0);    //color of circle positions during setup
+CRGB pulse = CRGB(0, 0, 0);       //color of goal position (placeholder)
 
 //Variables
-int playerLocation[2];
-int goalLocation[2];
-byte markerLocations[2][2];
-int mazeNum;
-bool markerBlinkState = true;
-bool playerBlinkState = true;
-unsigned long currentMillis;
-unsigned long previousMarkerMillis = 0;
-unsigned long previousPlayerMillis = 0;
-int dir;
-int markerBlinkCount = 0;
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+CRGB leds[NUM_GRID_LEDS];
+bool upState = false;
+bool upStateLast = false;
+bool leftState = false;
+bool leftStateLast = false;
+bool downState = false;
+bool downStateLast = false;
+bool rightState = false;
+bool rightStateLast = false;
+byte maze[11][11];
+byte mazeNum;
+byte playerLocation[2];
+byte goalLocation[2];
 
+const int maxPulse = 255; //max brightness of goal pulse
+const int minPulse = 100; //min brightness of goal pulse
 
-int getBtnDir()
+void updateButtons()
 {
-  if (digitalRead(UP_BTN) == 0)
-  {
-    return UP;
-  }
-  else if (digitalRead(DOWN_BTN) == 0)
-  {
-    return DOWN;
-  }
-  else if (digitalRead(LEFT_BTN) == 0)
-  {
-    return LEFT;
-  }
-  else if (digitalRead(RIGHT_BTN) == 0)
-  {
-    return RIGHT;
-  }
-  else
-  {
-    return -1;
-  }
+    //update debouncing
+    up_db.update();
+    left_db.update();
+    down_db.update();
+    right_db.update();
+
+    //update button states
+    upStateLast = upState;
+    upState = !up_db.read();
+    leftStateLast = leftState;
+    leftState = !left_db.read();
+    downStateLast = downState;
+    downState = !down_db.read();
+    rightStateLast = rightState;
+    rightState = !right_db.read();
 }
 
-void move(int direction)
+/*
+bool: 0 = x-axis, 1 = y-axis
+dir:  0 = down, 1 = up 
+*/
+void move()
 {
-  Serial.print("Trying to move ");
+    if (upState > upStateLast)
+    {
+        //check if move is in bounds
+        if (playerLocation[1] > 0)
+        { 
+            //check if move is valid
+            if (maze[playerLocation[1] * 2 - 1][playerLocation[0] * 2] == 1)
+            {
+                //strike if it isn't                  
+                module.strike();
+                leds[mazeMap[playerLocation[1] * 2 - 1][playerLocation[0] * 2]] = red;
+            }
+            else
+            {
+                leds[mainDotsMap[playerLocation[1]][playerLocation[0]]] = dark;
+                playerLocation[1] = playerLocation[1] - 1; //move if it is
+            }
+        }
+    }
 
-  switch (direction)
-  {
+    if (leftState > leftStateLast)
+    {
+        //check if move is in bounds
+        if (playerLocation[0] > 0)
+        { 
+            //check if move is valid
+            if (maze[playerLocation[1] * 2][playerLocation[0] * 2 - 1] == 1)
+            {   //strike if it isn't            
+                module.strike();
+                leds[mazeMap[playerLocation[1] * 2][playerLocation[0] * 2 - 1]] = red;
+            }
+            else
+            {
+                leds[mainDotsMap[playerLocation[1]][playerLocation[0]]] = dark;
+                playerLocation[0] = playerLocation[0] - 1; //move if it is
+            }
+        }
+    }
 
-  case UP:
-    Serial.println(F("up"));
-    if (mazeHorizontalWalls[mazeNum][playerLocation[0]][playerLocation[1]])
+    if (downState > downStateLast)
     {
-      module.strike();
-      Serial.println(F("Strike"));
+        //check if move is in bounds
+        if (playerLocation[1] < 5)
+        { 
+            //check if move is valid
+            if (maze[playerLocation[1] * 2 + 1][playerLocation[0] * 2] == 1)
+            {   //strike if it isn't                
+                module.strike();
+                leds[mazeMap[playerLocation[1] * 2 + 1][playerLocation[0] * 2]] = red;
+            }
+            else
+            {
+                leds[mainDotsMap[playerLocation[1]][playerLocation[0]]] = dark;
+                playerLocation[1] = playerLocation[1] + 1; //move if it is
+            }
+        }
     }
-    else
-    {
-      lc.setLed(0, playerLocation[0], playerLocation[1], false);
-      playerLocation[1] -= 1;
-      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-    }
-    break;
 
-  case DOWN:
-    Serial.println(F("down"));
-    if (mazeHorizontalWalls[mazeNum][playerLocation[0]][playerLocation[1] + 1])
+    if (rightState > rightStateLast)
     {
-      module.strike();
-      Serial.println(F("Strike"));
+        //check if move is in bounds
+        if (playerLocation[0] < 5)
+        { 
+            //check if move is valid
+            if (maze[playerLocation[1] * 2][playerLocation[0] * 2 + 1] == 1)
+            {   //strike if it isn't                  
+                module.strike();
+                leds[mazeMap[playerLocation[1] * 2][playerLocation[0] * 2 + 1]] = red;
+            }
+            else
+            {
+                leds[mainDotsMap[playerLocation[1]][playerLocation[0]]] = dark;
+                playerLocation[0] = playerLocation[0] + 1; //move if it is
+            }
+        }
     }
-    else
-    {
-      lc.setLed(0, playerLocation[0], playerLocation[1], false);
-      playerLocation[1] += 1;
-      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-    }
-    break;
 
-  case LEFT:
-    Serial.println(F("left"));
-    if (mazeVerticalWalls[mazeNum][playerLocation[1]][playerLocation[0]])
-    {
-      module.strike();
-      Serial.println(F("Strike"));
-    }
-    else
-    {
-      lc.setLed(0, playerLocation[0], playerLocation[1], false);
-      playerLocation[0] -= 1;
-      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-    }
-    break;
+    //update the lights
+    leds[mainDotsMap[playerLocation[1]][playerLocation[0]]] = white; 
+    leds[mainDotsMap[goalLocation[0]][goalLocation[1]]] = pulse;
 
-  case RIGHT:
-    Serial.println(F("right"));
-    if (mazeVerticalWalls[mazeNum][playerLocation[1]][playerLocation[0] + 1])
-    {
-      module.strike();
-      Serial.println(F("Strike"));
-    }
-    else
-    {
-      lc.setLed(0, playerLocation[0], playerLocation[1], false);
-      playerLocation[0] += 1;
-      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
-    }
-    break;
-
-  default:
-    break;
-  }
-  return;
+    FastLED.show();
 }
 
 void setup()
 {
-  Serial.begin(19200);
+    Serial.begin(19200);
 
-  Serial.println(F("Getting config"));
-  while (!module.getConfig())
-  {
-    module.interpretData();
-  }
-  Serial.println(F("Got config"));
+    Serial.println(F("Getting config"));
+    while (!module.getConfig())
+    {
+        module.interpretData();
+    }
+    Serial.println(F("Got config"));
 
-  Serial.println(F("Setting random seed"));
-  randomSeed(config_to_seed(module.getConfig()));
+    Serial.println(F("Setting random seed"));
+    randomSeed(config_to_seed(module.getConfig()));
 
-  Serial.println(F("Setting up I/O-pins"));
-  // 8x8 Led-matrix setup
-  pinMode(DATA_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(LOAD_PIN, OUTPUT);
+    Serial.println(F("Setting up I/O-pins"));
+    // 8x8 Led-matrix setup
+    pinMode(DATA_PIN, OUTPUT);
+    pinMode(CLOCK_PIN, OUTPUT);
 
-  //Directional-buttons setup
-  pinMode(UP_BTN, INPUT_PULLUP);
-  pinMode(DOWN_BTN, INPUT_PULLUP);
-  pinMode(LEFT_BTN, INPUT_PULLUP);
-  pinMode(RIGHT_BTN, INPUT_PULLUP);
+    //Directional-buttons setup
+    pinMode(UP_BTN, INPUT_PULLUP);
+    pinMode(DOWN_BTN, INPUT_PULLUP);
+    pinMode(LEFT_BTN, INPUT_PULLUP);
+    pinMode(RIGHT_BTN, INPUT_PULLUP);
 
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
-  lc.shutdown(0, false);
-  /* Set the brightness to a medium values */
-  lc.setIntensity(0, 8);
-  /* and clear the display */
-  lc.clearDisplay(0);
+    //Attatch debounce objects
+    right_db.attach(RIGHT_BTN);
+    right_db.interval(debounceTime);
+    up_db.attach(UP_BTN);
+    up_db.interval(debounceTime);
+    down_db.attach(DOWN_BTN);
+    down_db.interval(debounceTime);
+    left_db.attach(LEFT_BTN);
+    left_db.interval(debounceTime);
 
-  Serial.println(F("Choosing maze"));
-  mazeNum = random(0, 9);
-  Serial.println(F("Choose maze nr: "));
-  Serial.println(mazeNum);
+    FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, GRB>(leds, NUM_GRID_LEDS);
 
-  Serial.println(F("Getting marker locations"));
-  memcpy(markerLocations, markers[mazeNum], sizeof(markerLocations));
-  Serial.print(F("Marker 1 location: "));
-  Serial.print(markerLocations[0][0]);
-  Serial.print("\t");
-  Serial.println(markerLocations[0][1]);
+    Serial.println(F("Choosing maze"));
+    mazeNum = random(0, 9);
+    Serial.print(F("Choose maze nr: "));
+    Serial.println(mazeNum);
 
-  Serial.print(F("Marker 2 location: "));
-  Serial.print(markerLocations[1][0]);
-  Serial.print("\t");
-  Serial.println(markerLocations[1][1]);
+    Serial.println(F("Choosing player location"));
+    playerLocation[0] = random(0, 6);
+    playerLocation[1] = random(0, 6);
+    Serial.print(F("Player location: "));
+    Serial.print(playerLocation[0]);
+    Serial.print("\t");
+    Serial.println(playerLocation[1]);
 
-  Serial.println(F("Choosing player location"));
-  playerLocation[0] = random(0, 6);
-  playerLocation[1] = random(0, 6);
-  Serial.println(F("Player location: "));
-  Serial.print(playerLocation[0]);
-  Serial.print("\t");
-  Serial.println(playerLocation[1]);
+    Serial.println(F("Choosing goal location"));
+    do
+    {
+        goalLocation[0] = random(0, 6);
+        goalLocation[1] = random(0, 6);
+    } while (
+        ((goalLocation[0] == playerLocation[0]) && (goalLocation[1] == playerLocation[1])) ||
+        (abs((goalLocation[0] - playerLocation[0]) + (goalLocation[1] - playerLocation[1])) < 5));
 
-  Serial.println(F("Choosing goal location"));
-  do
-  {
-    goalLocation[0] = random(0, 6);
-    goalLocation[1] = random(0, 6);
-  } while (
-      ((goalLocation[0] == playerLocation[0])     && (goalLocation[1] == playerLocation[1]))      ||
-      ((goalLocation[0] == markerLocations[0][0]) && (goalLocation[1] == markerLocations[0][1]))  ||
-      ((goalLocation[0] == markerLocations[1][0]) && (goalLocation[1] == markerLocations[1][1]))  ||
-      (abs((goalLocation[0] - playerLocation[0]) + (goalLocation[1] - playerLocation[1])) < 5));
+    Serial.print(F("Goal location: "));
+    Serial.print(goalLocation[0]);
+    Serial.print("\t");
+    Serial.println(goalLocation[1]);
 
-  Serial.println(F("Goal location: "));
-  Serial.print(goalLocation[0]);
-  Serial.print("\t");
-  Serial.println(goalLocation[1]);
+    //fill in the maze matrix from storage
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            maze[i][j] = mazes[mazeNum][i][j];
+        }
+    }
 
-  Serial.println(F("Showing goal location"));
-  lc.setLed(0, goalLocation[0], goalLocation[1], true);
+    //clear matrix
+    for (int i = 0; i < NUM_GRID_LEDS; i++)
+    {
+        leds[i] = CRGB(0, 0, 0);
+    }
 
-  Serial.println(F("Setup done"));
-  module.sendReady();
+    //display circle positions and maze (if in DEBUG)
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = 0; j < 11; j++)
+        {
+            if (maze[i][j] == 2)
+            {
+                leds[mazeMap[i][j]] = dark;
+            }
+            if(maze[i][j] == 4)
+            {
+                leds[mazeMap[i][j]] = circle;
+            }
+#ifdef DEBUG
+            if (maze[i][j] == 1)
+            {
+                leds[mazeMap[i][j]] = darkred;
+            }
+#endif
+        }
+    }
+    FastLED.show();
+
+    Serial.println(F("Setup done"));
+    module.sendReady();
 }
 
 void loop()
 {
-  module.interpretData();
+    module.interpretData();
 
-  if (!module.is_solved)
-  {
-    //Blink maze markers and player location with different intervals
-    currentMillis = millis();
-
-    if (currentMillis - previousPlayerMillis >= 100)
+    if (!module.is_solved)
     {
-      // save the last time you blinked the player
-      if (markerBlinkCount >= 5)
-      {
-        markerBlinkState = !markerBlinkState;
-        markerBlinkCount = 0;
-      }
-      else
-      {
-        markerBlinkCount++;
-      }
+        updateButtons();
 
-      previousPlayerMillis = currentMillis;
-      playerBlinkState = !playerBlinkState;
+        //update pulse animation
+        pulse = CRGB(map(quadwave8(millis() / (20) * PI), 0, 255, minPulse, maxPulse), 0, 0); 
 
-      lc.setLed(0, markerLocations[0][0], markerLocations[0][1], markerBlinkState);
-      lc.setLed(0, markerLocations[1][0], markerLocations[1][1], markerBlinkState);
-      lc.setLed(0, playerLocation[0], playerLocation[1], playerBlinkState);
+        move();    
+    
+        //Check if player reached the goal
+        if ((playerLocation[0] == goalLocation[0]) && (playerLocation[1] == goalLocation[1]))
+        {
+            module.win();
+            Serial.println(F("Win"));
+        }
     }
-
-
-    if (millis() - lastDebounceTime >= 200)
-    {
-
-      //Read button press
-      dir = getBtnDir();
-      if (dir >= 0)
-      {
-        lastDebounceTime = millis();
-        Serial.print(F("Pressed: "));
-        Serial.println(dir);
-
-        //Try to move the player, gave a strike if a wall is hit
-        move(dir);
-
-        Serial.println(F("Player location: "));
-        Serial.print(playerLocation[0]);
-        Serial.print("\t");
-        Serial.println(playerLocation[1]);
-      }
-    }
-    //Check if player reached the goal
-    if ((playerLocation[0] == goalLocation[0]) && (playerLocation[1] == goalLocation[1]))
-    {
-      module.win();
-      Serial.println(F("Win"));
-    }
-  }
 }
