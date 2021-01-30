740efb8a36f89c8063b95d36ca2e146e48de7a86
sebski123 <sebthomle@gmail.com>
2021-01-30 01:22:26 +0100

    Updated moveLibs.sh to move all libs

--- MiscCode/moveLibs.sh
+++ MiscCode/moveLibs.sh
@@ -1,3 +1,3 @@
 #Moves Folders from Modules/Libraries to Arduino Library location
 cd MiscCode/Libraries
-cp -r SWire/ KTANECommon/ LedControl/ MAX6954/ C:/Users/sebth/Documents/Arduino/libraries
+cp -r */ C:/Users/sebth/Documents/Arduino/libraries
